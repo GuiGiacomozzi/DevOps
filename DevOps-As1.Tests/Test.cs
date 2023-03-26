61419da958971d04331c4b6242c4cddb3d8d522d
Guilherme Badeluk Giacomozzi <guigiacomozzi@outlook.com>
2023-03-26 14:40:36 +0100

    Added test project

---
+++ DevOps-As1.Tests/Test.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DevOps_As1.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
